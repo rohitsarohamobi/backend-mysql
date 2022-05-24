5bc2764c057f7e6827db74d857e8575837272a1e
rohit saroha <payalchadha>
2022-05-24 10:37:58 +0530

    cors policy added

--- app.js
+++ app.js
@@ -2,11 +2,18 @@ const express=require('express')
 const bodyParser=require('body-parser')
 const models = require('./models')
 const userRoutes=require('./routes/users')
+const cors = require('cors')
 
 const port=process.env.PORT || 5000
 require('dotenv').config()
 
 const app=express()
+app.use(cors({
+    origin: [
+        'http://localhost:3000',
+        'http://localhost:3001',
+    ]
+}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
